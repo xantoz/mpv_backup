0581f9eb2d22e336d9f1f7af731d2f30f76221b9
wm4 <wm4@nowhere>
2019-07-19 13:09:14 +0200

    Clarify license

--- demux/cache.c
+++ demux/cache.c
@@ -2,17 +2,17 @@
  * This file is part of mpv.
  *
  * mpv is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Affero General Public
- * License as published by the Free Software Foundation, either
- * version 3 of the License, or (at your option) any later version.
-
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
  * mpv is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Affero General Public License for more details.
-
- * You should have received a copy of the GNU Affero General Public
- * License along with mpv.  If not, see <https://www.gnu.org/licenses/>.
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with mpv.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 #include <errno.h>
